98a9c4b10da167ee9475b3769475f8a2dcbe87e1
Norman Johns <normcjohns@gmail.com>
2016-12-31 13:20:27 -0700

    first commit, learning

---
+++ learning with nick/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  learning with nick
+//
+//  Created by Norman Johns on 12/31/16.
+//  Copyright © 2016 Norman Johns. All rights reserved.
+// trial with github and source tree
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
