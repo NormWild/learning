de8f415015e70f63a25834820036f815aa2b50ea
Norman Johns <normcjohns@gmail.com>
2017-01-01 09:48:26 -0700

    add numbers

--- learning with nick/ViewController.swift
+++ learning with nick/ViewController.swift
@@ -11,6 +11,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var normLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UITextField!
+    
+    
+    @IBAction func Button(_ sender: Any) {
+       
+        normLabel.text = "answer is ...\(Double(text1.text!)! + Double(text2.text!)!)"
+       
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
